24a5d2f035bc47cf1d7f8bdf359024761ca15050
Unknown <315744068@qq.com>
2019-03-01 00:54:46 +0800

    UPDATE: 子组件下的reducer 设计和使用

---
+++ src/pages/home/store/reducer.js
@@ -0,0 +1,52 @@
+import { fromJS } from 'immutable'
+
+const defaultState = fromJS({
+    topicList: [
+        {
+            id: 1,
+            title: '社会热点',
+            imgURL:'https://upload.jianshu.io/users/upload_avatars/13213889/7314c5cc-ca7f-4542-b914-2c8dffaf324d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
+        },
+        {
+            id: 2,
+            title: '手绘',
+            imgURL:'https://upload.jianshu.io/users/upload_avatars/13213889/7314c5cc-ca7f-4542-b914-2c8dffaf324d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
+        },
+        {
+            id: 3,
+            title: '测试',
+            imgURL:'https://upload.jianshu.io/users/upload_avatars/13213889/7314c5cc-ca7f-4542-b914-2c8dffaf324d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
+        },
+        {
+            id: 4,
+            title: '不聚聚',
+            imgURL:'https://upload.jianshu.io/users/upload_avatars/13213889/7314c5cc-ca7f-4542-b914-2c8dffaf324d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
+        },
+        {
+            id: 5,
+            title: '列表内容',
+            imgURL:'https://upload.jianshu.io/users/upload_avatars/13213889/7314c5cc-ca7f-4542-b914-2c8dffaf324d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
+        },
+        {
+            id: 6,
+            title: 'reducer',
+            imgURL:'https://upload.jianshu.io/users/upload_avatars/13213889/7314c5cc-ca7f-4542-b914-2c8dffaf324d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
+        },
+        {
+            id: 7,
+            title: 'redux',
+            imgURL:'https://upload.jianshu.io/users/upload_avatars/13213889/7314c5cc-ca7f-4542-b914-2c8dffaf324d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
+        },
+        {
+            id: 8,
+            title: 'topics',
+            imgURL:'https://upload.jianshu.io/users/upload_avatars/13213889/7314c5cc-ca7f-4542-b914-2c8dffaf324d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp'
+        },
+    ]
+})
+
+export default (state = defaultState, action ) => {
+
+
+    return state;
+}

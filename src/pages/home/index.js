ee20d74eb91e7053029d1f553fd3dc9d8e8118e9
Unknown <315744068@qq.com>
2019-03-03 21:46:40 +0800

    UPDATE: 返回顶部功能

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,11 +5,17 @@ import Recommend from './components/Recommend';
 import Writer from './components/Writer';
 import { connect } from 'react-redux';
 import { actionCreators } from './store';
+import { BackTop } from './style';
 
 
 import { HomeWrapper , HomeLeft, HomeRight } from './style';
 
 class Home extends Component {
+
+    handleScrollToTop(){
+        window.scrollTo(0,0);
+    }
+
     render () {
         return (
             <HomeWrapper>
@@ -22,20 +28,45 @@ class Home extends Component {
                     <Recommend></Recommend>
                     <Writer></Writer>
                 </HomeRight>
+                {
+                    this.props.showScroll ? <BackTop onClick={this.handleScrollToTop}>回到顶部</BackTop>
+: null
+                }
             </HomeWrapper>
         );
     }
 
     componentDidMount(){
         this.props.changeHomeData();
+        this.bindEvents();
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.props.changeScrollTopShow)
+    }
+
+    bindEvents(){
+        window.addEventListener('scroll', this.props.changeScrollTopShow)
     }
 }
+
+const  mapState = (state) => ({
+    showScroll: state.getIn(['home','showScroll'])
+})
 
 const mapDispatch = (dispatch) => ({
     changeHomeData(){
         const action =  actionCreators.getHomeInfo();
         dispatch(action);
+    },
+    changeScrollTopShow(){
+
+        if(document.documentElement.scrollTop > 100){
+            dispatch(actionCreators.toggleTopShow(true));
+        }else{
+            dispatch(actionCreators.toggleTopShow(false));
+        }
     }
 })
 
-export default connect(null, mapDispatch)(Home);
+export default connect(mapState, mapDispatch)(Home);
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -11,23 +11,36 @@ const defaultState = fromJS({
     recommendList: [
     
     ],
-    articlePage: 1
+    articlePage: 1,
+    showScroll: false
 })
+
+const changeHomeData = (state, action) => {
+    return state.merge({
+        topicList: fromJS(action.topicList),
+        articleList: fromJS(action.articleList),
+        recommendList: fromJS(action.recommendList)
+    })
+}
+
+const addArticleList = (state, action) => {
+    return state.merge({
+        articleList: state.get('articleList').concat(action.list),
+        articlePage: action.nextPage
+    });
+}
 
 export default (state = defaultState, action ) => {
     if(action.type === constants.CHANGE_HOME_DATA){
-        return state.merge({
-            topicList: fromJS(action.topicList),
-            articleList: fromJS(action.articleList),
-            recommendList: fromJS(action.recommendList)
-        })
+        return changeHomeData(state, action);
     }
 
     if(action.type === constants.ADD_ARTICLE_LIST){
-        return state.merge({
-            articleList: state.get('articleList').concat(action.list),
-            articlePage: action.nextPage
-        });
+        return addArticleList(state, action);
+    }
+
+    if(action.type === constants.TOGGLE_SCROLL_TOP){
+        return state.set('showScroll', action.show);
     }
 
     
